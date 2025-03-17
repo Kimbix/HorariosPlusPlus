9a04402da3d9660802c7044b55f6eba3ab5f0a80
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2025-03-16 21:10:16 -0400

    Change == for === when expected in hopes of fixing warnings

--- src/App.tsx
+++ src/App.tsx
@@ -194,7 +194,7 @@ function SectionComp({ visibleOverride, sectionObject, removeCallback }: { visib
 	}
 
 	function visibleToggle() {
-		if (visibleOverride == false) return;
+		if (visibleOverride === false) return;
 		const newSection: Section = { ...sectionObject };
 		sectionObject.visible = !sectionObject.visible
 		newSection.visible = newSection.visible
@@ -202,7 +202,7 @@ function SectionComp({ visibleOverride, sectionObject, removeCallback }: { visib
 	}
 
 	function isVisible() {
-		if (visibleOverride == false) return false
+		if (visibleOverride === false) return false
 		return sectionObject.visible
 	}
 
@@ -349,7 +349,7 @@ function Schedule({ id, sections }: { id: number, sections: ScheduleSection[] })
 
 	function padNumber(n: Number): String {
 		const num = String(n)
-		return String(((num.length == 1 || "") && "0") + num);
+		return String(((num.length === 1 || "") && "0") + num);
 	}
 
 	function handleMouseMove(e: MouseEvent) {
